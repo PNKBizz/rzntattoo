8abaf0cb5ab9b61d708e624f0f6dcd4a43f66b24
Pavel Galkin <pavel_galkin@epam.com>
2017-04-25 16:31:25 +0300

    start implementing layout

--- server.js
+++ server.js
@@ -16,22 +16,6 @@ app.get('/api/masters', function(req, res) {
   });
 });
 
-app.get('/api/update', async function(req, res) {
-  try {
-    const masters = await fs.readdir('./src/assets/gallery');
-    const galleryPromises = await masters.map(async master => {
-      const gallery = await fs.readdir('./src/assets/gallery/' + master);
-      return { master: master, gallery: gallery }
-    });
-    const response = await Promise.all(galleryPromises);
-    jsonfile.writeFile('./src/assets/masters.json', {masters: response}, function (err) {
-      if (process.env.NODE_ENV !== 'production') console.error(err)
-    });
-  } catch(ex) {
-    if (process.env.NODE_ENV !== 'production') console.error(ex);
-  }
-});
-
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 4001, function() {
     console.log('App listening on port 4001')
