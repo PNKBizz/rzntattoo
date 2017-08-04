5a7a2a498057f531bbebe0503fa8c1444b7c5f7a
Pavel_Galkin <pavel_galkin@epam.com>
2017-08-04 17:40:23 +0300

    Refactoring lazy directive

--- src/directives/lazy.js
+++ src/directives/lazy.js
@@ -1,24 +1,31 @@
+let images = [];
+
+document.addEventListener('scroll', () => {
+  for (let image of images) {
+    checkEl(image.el, image.binding)
+  }
+});
+
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
   const html = document.documentElement;
   return (
-    rect.top >= 0 && rect.left >= 0 &&
-    (window.innerHeight || html.clientHeight) - rect.top >= 0 &&
-    (window.innerWidth || html.clientWidth) - rect.right >= 0
+    rect.top >= 0 &&
+    (window.innerHeight || html.clientHeight) - rect.top >= 0
   );
+}
+
+function checkEl(el, binding) {
+  if (isInViewport(el)) {
+    el.src = binding.value;
+  }
 }
 
 export default {
   bind(el, binding) {
-    document.addEventListener('scroll', () => {
-      if (isInViewport(el)) {
-        el.src = binding.value;
-      }
-    });
+    images.push({ el, binding })
   },
   inserted(el, binding) {
-    if (isInViewport(el)) {
-      el.src = binding.value;
-    }
-  },
+    checkEl(el, binding);
+  }
 };
