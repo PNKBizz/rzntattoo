ad344c263ec3741ee26e516c40725945f105c2d6
pavelgalkin <pavel_galkin@epam.com>
2017-11-18 13:24:25 +0300

    server refactoing

--- server.js
+++ server.js
@@ -1,37 +1,10 @@
-var express = require('express'),
+const express = require('express'),
 	app = express(),
-	bodyParser = require('body-parser'),
-	jsonfile = require('jsonfile'),
-	fs = require('mz/fs');
+	router = require('./router');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(express.json());
 
-app.get('/api/masters', function (req, res) {
-	jsonfile.readFile('./src/assets/masters.json', function (err, data) {
-		if (err && process.env.NODE_ENV !== 'production') console.error(err);
-		res.send(data);
-	});
-});
-
-app.get('/api/update', function (req, res) {
-	fs.readdir('./src/assets/gallery').then(masters => {
-		const galleryPromises = masters.map(master => {
-			return fs.readdir('./src/assets/gallery/' + master);
-		});
-		Promise.all(galleryPromises).then((response) => {
-			const toWrite = masters.map((master, i) => {
-				return {name: master, gallery: response[i].filter(file => file !== 'master.jpg' && file !== 'thumbs')}
-			});
-			jsonfile.writeFile('./src/assets/masters.json', {masters: toWrite}, function (err) {
-				if (process.env.NODE_ENV !== 'production') console.error(err);
-				res.end();
-			});
-		});
-	});
-});
+app.use('/', router);
 
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 4001, function () {
