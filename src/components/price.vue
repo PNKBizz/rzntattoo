159ee987e5cfb56174512361b685b815263219fc
Pavel Galkin <bizz.gc@gmail.com>
2019-10-04 09:26:45 +0300

    change prices

--- src/components/price.vue
+++ src/components/price.vue
@@ -2,9 +2,9 @@
     <section class="container-fluid full-height price-back">
         <div class="row justify-content-center align-items-end full-height">
             <div class="price-table col-md-10 col-lg-6">
-                <h1>Татуировка</h1>
-                <p>Час работы мастера стоит <span class="red">4000 руб.</span> Минимальная стоимость работы - 2000 руб.</p>
-                <h1>Пирсинг</h1>
+                <h1>Сколько стоит татуировка?</h1>
+                <p>Здравствуйте, <span class="red">2000р.</span> минимальная стоимость, далее оплата по сеансам. Час работы - <span class="red">3000р.</span>, 2 часа - <span class="red">6000р.</span>, 3х часовой сеанс - <span class="red">8500р.</span> На большие сеансы действует скидка, например 5-ти часовой сеанс уже будет стоить <span class="red">13000р.</span>, а не <b>15000р.</b> Длительность сеанса будет зависеть от размера, детализации и сложности рисунка, и места нанесения.</p>
+                <h1>А пирсинг?</h1>
                 <p>Стоимость пирсинга - <span class="red">от 500р до 1500р.</span> в зависимости от места и сложности.</p>
             </div>
         </div>
@@ -23,5 +23,9 @@
         color: white;
         padding: 50px;
         height: 90%;
+    }
+
+    .price-table p {
+        line-height: 2;
     }
 </style>
